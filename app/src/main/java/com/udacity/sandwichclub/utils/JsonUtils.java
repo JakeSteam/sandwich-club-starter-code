9b57cdf8a8666e7e67738922933bae60e8b9cc5d
Jake Lee <github@jakelee.co.uk>
2019-02-12 21:36:35 +0000

    JSON parsing added

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,38 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            return new Sandwich(
+                    sandwichJson.getJSONObject("name").getString("mainName"),
+                    jsonArrayToList(sandwichJson.getJSONObject("name").getJSONArray("alsoKnownAs")),
+                    sandwichJson.getString("placeOfOrigin"),
+                    sandwichJson.getString("description"),
+                    sandwichJson.getString("image"),
+                    jsonArrayToList(sandwichJson.getJSONArray("ingredients"))
+            );
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    private static List<String> jsonArrayToList(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> list = new ArrayList<>();
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.length(); i++){
+                list.add(jsonArray.getString(i));
+            }
+        }
+        return list;
     }
 }
